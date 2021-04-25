66cc887350b7a5ec08817a94358b3e1a359bc7cc
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-04-25 11:01:31 -0300

    header working

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles.css';
 
 export default function Header() {
 
     return(
         <div className='header'>
-            <h1>Esse é o header</h1>
-            <a href="/">Login</a>
-            <a href="/tasks">Tasks</a>
+            <Link className='header__logo' to="/">
+                <img className='header__logo-img' src='logo.png' alt='Logotipo do Tasky' />
+            </Link>
+            <button className='header__login'>Login</button>
+            <Link className='header__tasks' to="/tasks">Minhas Tarefas</Link>
         </div>
     );
 }
