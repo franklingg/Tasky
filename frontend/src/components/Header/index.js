5af5faa91a9d676e2894fbb0b75a8a05b4f9e782
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-04-25 12:45:09 -0300

    register/login form working

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './styles.css';
 
-export default function Header() {
+export default function Header(props) {
 
     return(
         <div className='header'>
             <Link className='header__logo' to="/">
                 <img className='header__logo-img' src='logo.png' alt='Logotipo do Tasky' />
             </Link>
-            <button className='header__login'>Login</button>
+            <button className='header__login' onClick={props.toggler}>{props.isRegister? 'Login':'Register'}</button>
             <Link className='header__tasks' to="/tasks">Minhas Tarefas</Link>
         </div>
     );
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import Header from '../../components/Header';
+import './styles.css';
+import api from '../../service/api';
 
 export default function Home() {
+    const [registerForm, setRegisterForm] = React.useState(true);
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const toggleForm = ()=> setRegisterForm(!registerForm);
+    const changeName = event => setName(event.target.value);
+    const changeEmail = event => setEmail(event.target.value);
+    const changePassword = event => setPassword(event.target.value);
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        const data = {name, email, password};
+        
+        const URI = '/users/' + (registerForm ? 'register' : 'login');
+        api.post(URI,data)
+        .then(res=>alert(res.status))
+        .catch(err=> alert(err.response.data.error));
+    }
+
     return(
         <>
-            <Header/>
-            <h1>
-                this is the home
-            </h1>
+            <Header toggler={toggleForm} isRegister={registerForm}/>
+            <main className='home'>
+                <img className='home__img' src='home.svg' alt='Imagem da página principal do Tasky'/>
+                <form className='home__register' onSubmit={handleSubmit}>
+                    {registerForm ?
+                    <>
+                    <h2 className='home__form-title'>Cadastre-se</h2>
+                    <label>Nome</label>
+                    <input type="text" value={name} onChange={changeName} />
+                    </> :
+                    <h2 className='home__form-title'>Faça Login</h2>
+                    }
+                    <label>Email</label>
+                    <input type="email" value={email} onChange={changeEmail} />
+                    <label>Senha</label>
+                    <input type="password" value={password} onChange={changePassword} />
+                    <input type="submit" value="Entrar" />
+                </form>
+            </main>
         </>
     )
 } 
