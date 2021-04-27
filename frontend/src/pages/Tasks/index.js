337a8e6373f45a4ac4657e87162b0c7b34ba6aba
Augusto Nunes Zacarias <augustonz2010@hotmail.com>
2021-04-27 09:34:07 -0300

    Added tasks

--- frontend/src/pages/Tasks/index.js
+++ frontend/src/pages/Tasks/index.js
@@ -6,10 +6,31 @@ import './styles.css';
 export default function Tasks() {
     
     const [tasks,setTasks] = useState([]);
-    
+    const [selectedTask,setSelectedTask] = useState(null);
+    const [formValues,setFormValues] = useState({name:"",prioridade:""});
+
+    function handleTaskFocus(event,task) {
+        console.log(event.target);
+        console.log(task);
+    }
+
+    function changeSorting(event) {
+
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formValues)
+    }
+
+    function handleUpdate(event) {
+        const Name=event.target.name;
+        const value=event.target.value;
+        setFormValues({...formValues,[Name]:value});
+    }
+
     useEffect(async()=>{
         const response = await api.get('api/?results=10');
-        console.log(response.data);
         setTasks(response.data.results);
     },[])
     
@@ -20,18 +41,55 @@ export default function Tasks() {
                 <div className="tasksContainer">
                     {tasks.map(task=>{
                         return(
-                            <div className="task">
-                                {task.name.first} {task.name.last}
+                            <div key={task.name.first} onClick={(event)=>handleTaskFocus(event,task)}>
+                                Tarefa: {task.name.first} {task.name.last}<br/>
+                                Prioridade: 
                             </div>
                         );
                     })}
                 </div>
-                
-                {/*
+                {
                 <div>
-                    <SortTasks/>
-                    <AddNewTaks/>
-                </div>*/}
+                    <div className="sortTasksContainer">
+                        Ordernar tarefas por: 
+                        <div>
+                            <label> 
+                                <input type="radio" name="sort" id="data"  value="data" onChange={changeSorting} checked/>
+                                Data de criação
+                            </label>
+                        </div>
+
+                        <div>
+                            <label>
+                                
+                                <input type="radio"  name="sort" id="priority" value="priority" onChange={changeSorting}/>
+                                Prioridade
+                            </label>
+                        </div>
+                    </div>
+                    <div className="newTasksContainer">
+                        Criar nova tarefa<br/>
+                        <form onSubmit={handleSubmit}>
+                            <label>
+                                Nome:
+                                <input type="text" name="name" required onChange={handleUpdate} value={formValues.name}/>
+                            </label>
+                            <div>
+                                <label>
+                                    <input type="radio"  name="prioridade" onChange={handleUpdate} value="alta" checked={formValues.prioridade==="alta"}/>
+                                    Alta
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <input type="radio"  name="prioridade" onChange={handleUpdate} value="baixa" checked={formValues.prioridade==="baixa"}/>
+                                    Baixa
+                                </label>
+                            </div>
+                            <button type="submit">Criar tarefa</button>
+                        </form>
+                    </div>
+                </div>}
             </div>
         </>
     )
